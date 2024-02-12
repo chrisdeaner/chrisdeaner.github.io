71cde5d71f872f46c25ce9e311a619c3803e26dc
Chris Deaner <chris@heardsounds.com>
2024-02-12 09:46:18 -0500

    import all shows

--- scripts/showsToJson.mjs
+++ scripts/showsToJson.mjs
@@ -13,27 +13,34 @@ async function processLineByLine() {
 	let monthYear = "";
 	for await (const line of rl) {
 		const show = {};
-		// Each line in input.txt will be successively available here as `line`.
+		// each line in input.txt will be successively available here as `line`.
 		const day = line.slice(0, 2);
-		const theRest = line.slice(2);
 		// if it is not a number, then it's a month / year
 		if (isNaN(day)) {
 			monthYear = line;
 			continue;
 		}
-		const epoch = Date.parse(`${day} ${monthYear}`);
-		const splitByParen = theRest.split("(");
-		show.epoch = epoch;
-		show.venue = splitByParen[0].trim();
-		show.location = splitByParen[1].split(")")[0];
-		show.lineup = splitByParen[1].split(")")[1];
+		show.date = `${day} ${monthYear}`;
+
+		// rest of the line
+		const theRest = line.slice(2);
+		// get the venue (everything until the first open paren)
+		let splitByParen = theRest.split("(");
+		// remove from array
+		show.venue = splitByParen.splice(0, 1)[0].trim();
+		// create a string from the rest of the array
+		const locationAndLineup = splitByParen.join("(");
+		// get location and lineup
+		let lineup = locationAndLineup.split(")");
+		show.location = lineup.splice(0, 1)[0];
+		show.lineup = lineup.join(")");
 		shows.push(show);
 	}
 }
 
 await processLineByLine();
 
-fs.writeFile("../assets/json/shows.json", JSON.stringify(shows), (err) => {
+fs.writeFile("../_data/shows.json", JSON.stringify(shows), (err) => {
 	// Checking for errors
 	if (err) throw err;
 });
